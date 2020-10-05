b9e2d657ccaac9738d88b9432d5c8b066428e101
Kristoffer Lehre Fagerheim <kristoffer.lf@gmail.com>
2020-10-05 13:37:30 +0200

    Cloning project from existing repository

--- calfem-python-master/examples_TMM4135/quads.py
+++ calfem-python-master/examples_TMM4135/quads.py
@@ -1,68 +1,362 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 21 08:15:51 2018
-
 @author: bjohau
 """
 import numpy as np
+import sys
 
-def quad4e(ex,ey,D,th,eq=None):
+def gauss_points(iRule):
+    """
+    Returns gauss coordinates and weight given integration number
+
+    Parameters:
+
+        iRule = number of integration points
+
+    Returns:
+
+        gp : row-vector containing gauss coordinates
+        gw : row-vector containing gauss weight for integration point
+    """
+
+    gauss_position = [[ 0.000000000],
+                      [-0.577350269,  0.577350269],
+                      [-0.774596669,  0.000000000,  0.774596669],
+                      [-0.8611363116, -0.3399810436, 0.3399810436, 0.8611363116],
+                      [-0.9061798459, -0.5384693101, 0.0000000000, 0.5384693101, 0.9061798459]]
+    gauss_weight   = [[2.000000000],
+                      [1.000000000,   1.000000000],
+                      [0.555555556,   0.888888889,  0.555555556],
+                      [0.3478548451,  0.6521451549, 0.6521451549, 0.3478548451],
+                      [0.2369268850,  0.4786286705, 0.5688888889, 0.4786286705, 0.2369268850]]
+
+
+    if iRule < 1 and iRule > 5:
+        sys.exit("Invalid number of integration points.")
+
+    idx = iRule - 1
+    return gauss_position[idx], gauss_weight[idx]
+
+
+def quad4_shapefuncs(xsi, eta):
+    
+    """
+    Calculates shape functions evaluated at xsi and eta
+    Uses zero-lines to find the shape 
+
+    Parameters:
+        xsi = Coordinate along the xsi-axis
+        eta = Coordinate along the eta-axis
+        
+    Returns:
+        N   = Array of shapefunctions for the given quad
+    """
+    
+    # ----- Shape functions -----
+    N    = np.zeros(4)
+    N[0] = (1/4) * (1 + xsi) * (1 + eta)
+    N[1] = (1/4) * (1 - xsi) * (1 + eta)
+    N[2] = (1/4) * (1 - xsi) * (1 - eta)
+    N[3] = (1/4) * (1 + xsi) * (1 - eta)
+    return N
+
+
+def quad4_shapefuncs_grad_xsi(xsi, eta):
+
+    """
+    Calculates derivatives of shape functions wrt. xsi from quad4_shapefuncs_grad
+    Uses the chain rule to find the derivation of N with respect to eta
+
+    Parameters:
+        xsi  = Coordinate along the xsi-axis
+        eta  = Coordinate along the eta-axis
+        
+    Returns:
+        Ndxi = Array of the derivated shapefunctions with respect to xsi for the given quad
+    """
+    
+    # ----- Derivatives of shape functions with respect to xsi -----
+    Ndxi    = np.zeros(4)
+    Ndxi[0] = (1/4) * (1 + eta)
+    Ndxi[1] = (1/4) * (1 + eta) * (-1)
+    Ndxi[2] = (1/4) * (1 - eta) * (-1)
+    Ndxi[3] = (1/4) * (1 - eta)
+    return Ndxi
+
+
+def quad4_shapefuncs_grad_eta(xsi, eta):
+
+    """
+    Calculates derivatives of shape functions wrt. eta from quad4_shapefuncs_grad
+    Uses the chain rule to find the derivation of N with respect to eta
+
+    Parameters:
+        xsi  = Coordinate along the xsi-axis
+        eta  = Coordinate along the eta-axis
+        
+    Returns:
+        Ndxi = Array of the derivated shapefunctions with respect to eta for the given quad
+    """
+    
+    # ----- Derivatives of shape functions with respect to eta -----
+    Ndeta    = np.zeros(4)
+    Ndeta[0] = (1/4) * (1 + xsi)
+    Ndeta[1] = (1/4) * (1 - xsi)
+    Ndeta[2] = (1/4) * (1 - xsi) * (-1)
+    Ndeta[3] = (1/4) * (1 + xsi) * (-1)
+    return Ndeta
+
+
+def quad4e(ex, ey, D, thickness, eq=None):
+
     """
     Compute the stiffness matrix for a four node membrane element.
 
+    Parameters:
     :param list ex: element x coordinates [x1, x2, x3]
     :param list ey: element y coordinates [y1, y2, y3]
     :param list D : 2D constitutive matrix
     :param list th: element thickness
     :param list eq: distributed loads, local directions [bx, by]
-    :return mat Ke: element stiffness matrix [6 x 6]
-    :return mat fe: consistent load vector [6 x 1] (if eq!=None)
+
+    Returns:
+    :return mat Ke: element stiffness matrix [8 x 8]
+    :return mat fe: consistent load vector [8 x 1] (if eq!=None)
     """
 
-    ex1 = ex[0:3]
-    ex2 = ex[1:4]
-    ey1 = ey[0:3]
-    ey2 = ey[1:4]
+    t = thickness
 
-    Ke = np.matrix(np.zeros((8,8)))
-    fe = np.matrix(np.zeros((8,1)))
-
-    # TODO: fill out missing parts (or reformulate completely)
-
-    if eq is None:
-        return Ke
+    if eq is 0:
+        f = np.zeros((2,1))   # Create zero matrix for load if load is zero
     else:
-        fe[:6,0]  += ft1
-        fe[2:8,0] += ft2
-        return Ke, fe
+        f = np.array([eq]).T  # Convert load to 2x1 matrix
+
+    Ke = np.zeros((8,8))        # Create zero-matrix for stiffness-matrix
+    fe = np.zeros((8,1))        # Create zero-matrix for distributed load
+
+    numGaussPoints = 2                     # Number of integration points
+    gp, gw = gauss_points(numGaussPoints)  # Get integration points and -weight
+
+    for iGauss in range(numGaussPoints):   # Solves for K and fe at all integration points
+        for jGauss in range(numGaussPoints):
+
+            xsi = gp[iGauss]
+            eta = gp[jGauss]
+
+            # Collect shape functions evaluated at xi and eta
+            Ndxsi = quad4_shapefuncs_grad_xsi(xsi, eta)
+            Ndeta = quad4_shapefuncs_grad_eta(xsi, eta)
+            N1    = quad4_shapefuncs(xsi, eta)
+
+            # Matrix H and G defined according to page 52 of Waløens notes
+            H = np.transpose([ex, ey])    # Collect global x- and y coordinates in one matrix
+            G = np.array([Ndxsi, Ndeta])  # Collect gradients of shape function evaluated at xi and eta
+
+            # Calculates the Jacobian-matrix by taking the matrix multiplication of G and H
+            J = G @ H
+            # Defines the inverse Jacobian-matrix
+            invJ = np.linalg.inv(J)  # Inverse of Jacobian
+            # Defines the determinant of the Jacobian-matrix
+            detJ = np.linalg.det(J)  # Determinant of Jacobian
+
+            # Derivatives of shape functions with respect to x and y
+            dN = invJ @ G
+            dNdx = dN[0]
+            dNdy = dN[1]
+
+            # Strain displacement matrix calculated at position xsi, eta
+
+            # Sets the correct values for strain displacement matrix at current xsi and eta
+            B  = np.array([[dNdx[0], 0, dNdx[1], 0, dNdx[2], 0, dNdx[3], 0],
+                            [0, dNdy[0], 0, dNdy[1], 0, dNdy[2], 0, dNdy[3]],
+                            [dNdy[0], dNdx[0], dNdy[1], dNdx[1], dNdy[2], dNdx[2], dNdy[3], dNdx[3]]])
+
+            # Sets the correct values for displacement interpolation xsi and eta
+            zeroMatrix = np.zeros((4))
+            N2 = np.zeros((2, 8))
+            N2[0] = np.concatenate([N1, zeroMatrix])
+            N2[1] = np.concatenate([zeroMatrix, N1])
+
+            # Evaluates integrand at current integration points and adds to final solution
+            Ke += (B.T) @ D @ B * detJ * t * gw[iGauss] * gw[jGauss]
+            fe += (N2.T) @ f    * detJ * t * gw[iGauss] * gw[jGauss]
+    
+    return Ke, fe  # Returns stiffness matrix and nodal force vector
+
+def quad9_shapefuncs(xsi, eta):
+
+    """
+    Calculates shape functions evaluated at xsi and eta
+    Uses zero-lines to find the shape functions
+
+    Parameters:
+        xsi  = Coordinate along the xsi-axis
+        eta  = Coordinate along the eta-axis
+        
+    Returns:
+        Ndxi = Array of shapefunctions for the given quad
+    """
+    
+    # ----- Shape functions -----
+    N    = np.zeros(9)
+    N[0] =   (1/4) * (xsi+xsi**2) * (eta+eta**2)
+    N[1] = - (1/4) * (xsi-xsi**2) * (eta+eta**2)
+    N[2] =   (1/4) * (xsi-xsi**2) * (eta-eta**2)
+    N[3] = - (1/4) * (xsi+xsi**2) * (eta-eta**2)
+    N[4] =   (1/2) * (eta+eta**2) * (1-xsi**2)
+    N[5] = - (1/2) * (xsi-xsi**2) * (1-eta**2)
+    N[6] = - (1/2) * (eta-eta**2) * (1-xsi**2)
+    N[7] =   (1/2) * (xsi+xsi**2) * (1+eta**2)
+    N[8] =   (1-eta**2) * (1-xsi**2)
+    return N
 
 
+def quad9_shapefuncs_grad_xsi(xsi, eta):
+
+    """
+    Calculates derivatives of shape functions wrt. xsi from quad4_shapefuncs_grad
+    Uses the chain rule to find the derivation of N with respect to xsi
+
+    Parameters:
+        xsi  = Coordinate along the xsi-axis
+        eta  = Coordinate along the eta-axis
+        
+    Returns:
+        Ndxi = Array of the derivated shapefunctions with respect to xsi for the given quad
+    """
+    
+    # ----- Derivatives of shape functions with respect to xsi -----
+    Ndxi    = np.zeros(9)
+    Ndxi[0] =   (1/4) * (eta+eta**2) * (1+2*xsi)
+    Ndxi[1] = - (1/4) * (eta+eta**2) * (1-2*xsi)
+    Ndxi[2] =   (1/4) * (eta-eta**2) * (1-2*xsi)
+    Ndxi[3] = - (1/4) * (eta-eta**2) * (1+2*xsi)
+    Ndxi[4] = -  xsi  * (eta+eta**2)
+    Ndxi[5] = - (1/2) *  (1-eta**2)  * (1-2*xsi)
+    Ndxi[6] =    xsi  * (eta-eta**2)
+    Ndxi[7] =   (1/2) *  (1-eta**2)  * (1+2*xsi)
+    Ndxi[8] = -  xsi  *  (1-eta**2)  *     2
+    return Ndxi
+
+
+def quad9_shapefuncs_grad_eta(xsi, eta):
+    
+    """
+    Calculates derivatives of shape functions wrt. eta from quad4_shapefuncs_grad
+    Uses the chain rule to find the derivation of N with respect to eta
+
+    Parameters:
+        xsi  = Coordinate along the xsi-axis
+        eta  = Coordinate along the eta-axis
+        
+    Returns:
+        Ndxi = Array of the derivated shapefunctions with respect to eta for the given quad
+    """
+
+    # ----- Derivatives of shape functions with respect to eta -----
+    Ndeta    = np.zeros(9)
+    Ndeta[0] =   (1/4) * (xsi+xsi**2) * (1+2*eta)
+    Ndeta[1] = - (1/4) * (xsi-xsi**2) * (1+2*eta)
+    Ndeta[2] =   (1/4) * (xsi-xsi**2) * (1-2*eta)
+    Ndeta[3] = - (1/4) * (xsi+xsi**2) * (1-2*eta)
+    Ndeta[4] =   (1/2) *  (1-xsi**2)  * (1+2*eta)
+    Ndeta[5] =    eta  * (xsi-xsi**2)
+    Ndeta[6] = - (1/2) *  (1-xsi**2)  * (1-2*eta)
+    Ndeta[7] = -  eta  * (xsi+xsi**2)
+    Ndeta[8] = -  eta  *  (1-xsi**2)  *     2
+    return Ndeta
 
 def quad9e(ex,ey,D,th,eq=None):
-    """
-    Compute the stiffness matrix for a four node membrane element.
 
+    """
+    Compute the stiffness matrix for a nine node membrane element.
+
+    Parameters:
     :param list ex: element x coordinates [x1, x2, x3]
     :param list ey: element y coordinates [y1, y2, y3]
     :param list D : 2D constitutive matrix
     :param list th: element thickness
     :param list eq: distributed loads, local directions [bx, by]
-    :return mat Ke: element stiffness matrix [6 x 6]
-    :return mat fe: consistent load vector [6 x 1] (if eq!=None)
+
+    Returns:
+    :return mat Ke: element stiffness matrix [18 x 18]
+    :return mat fe: consistent load vector [18 x 1] (if eq!=None)
     """
 
-    Ke = np.matrix(np.zeros((18,18)))
-    fe = np.matrix(np.zeros((18,1)))
+    t = th
 
-    # TODO: fill out missing parts (or reformulate completely)
+    if eq is 0:
+        f = np.zeros((2,1))                     # Create zero matrix for load if load is zero
+    else:
+        f = np.array([eq]).T                    # Convert load to 2x1 matrix
 
+    Ke = np.matrix(np.zeros((18,18)))           # Create zero-matrix for stiffness-matrix
+    fe = np.matrix(np.zeros((18,1)))            # Create zero-matrix for distributed load
+
+    numGaussPoints = 2                          # Number of integration points
+    gp, gw = gauss_points(numGaussPoints)       # Get integration points and -weight
+
+    for iGauss in range(numGaussPoints):        # Solves for K and fe at all integration points
+        for jGauss in range(numGaussPoints):
+
+            xsi = gp[iGauss]
+            eta = gp[jGauss]
+
+            # Collect shape functions evaluated at xi and eta
+            Ndxsi = quad9_shapefuncs_grad_xsi(xsi, eta)
+            Ndeta = quad9_shapefuncs_grad_eta(xsi, eta)
+            N1    = quad9_shapefuncs(xsi, eta)
+
+            # Matrix H and G defined according to page 52 of Waløens notes
+            H = np.transpose([ex, ey])          # Collect global x- and y coordinates in one matrix
+            G = np.array([Ndxsi, Ndeta])        # Collect gradients of shape function evaluated at xi and eta
+
+            # Calculates the Jacobian-matrix by taking the matrix multiplication of G and H
+            J = G @ H
+            # Defines the inverse Jacobian-matrix
+            invJ = np.linalg.inv(J)             # Inverse of Jacobian
+            # Defines the determinant of the Jacobian-matrix
+            detJ = np.linalg.det(J)             # Determinant of Jacobian
+
+            # Derivatives of shape functions with respect to x and y
+            dN = invJ @ G
+            dNdx = dN[0]
+            dNdy = dN[1]
+
+            # Strain displacement matrix calculated at position xsi, eta
+
+            # Sets the correct values for strain displacement matrix at current xsi and eta
+            B  = np.zeros((3, 18))
+            j = 0
+            g = 0
+            h = 0
+            for i in range(0, 18):
+                if (i % 2) == 0:
+                    B[0, i] = dNdx[j]
+                    B[1, i] = 0
+                    B[2, i] = dNdy[h]
+                    j += 1
+                else:
+                    B[0, i] = 0
+                    B[1, i] = dNdy[g]
+                    B[2, i] = dNdx[h]
+                    g += 1
+                    h += 1
+
+            # Sets the correct values for displacement interpolation xsi and eta
+            zeroMatrix = np.zeros((9))
+            N2 = np.zeros((2, 18))
+            N2[0] = np.concatenate([N1, zeroMatrix])
+            N2[1] = np.concatenate([zeroMatrix, N1])
+
+            # Evaluates integrand at current integration points and adds to final solution
+            Ke += (B.T)  @ D @ B * detJ * t * gw[iGauss] * gw[jGauss]
+            fe += (N2.T) @ f     * detJ * t * gw[iGauss] * gw[jGauss]
+
+    # Returns Ke if load is zero
     if eq is None:
         return Ke
+    # Returns Ke and Fe
     else:
-        return Ke, fe
-
-
-
-
-
-  
+        return Ke, fe  
--- calfem-python-master/examples_TMM4135/triangles.py
+++ calfem-python-master/examples_TMM4135/triangles.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 21 08:15:51 2018
 
-@author: bjohau
+"""
+@author: simen og kristoffer
 """
 import numpy as np
 
@@ -37,32 +35,28 @@ def tri3e(ex,ey,D,th,eq=None):
                      [1,ex[2],ey[2]]])
     
     A2 = np.linalg.det(tmp)  # Double of triangle area
+
     A  = A2 / 2.0
        
-    cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
-
-    # TODO: fill out missing parts (or reformulate completely)
-
-    ''' MY CODE '''
- 
-    zeta_px, zeta_py = zeta_partials_x_and_y(ex,ey)
+    zeta_px, zeta_py = zeta_partials_x_and_y(ex,ey) #Partial derivatives of zeta with respect to x and y 
     
-    B  = (1/A2)*np.array([[zeta_px[0], 0, zeta_px[1], 0, zeta_px[2], 0],
+    B  = np.array([[zeta_px[0], 0, zeta_px[1], 0, zeta_px[2], 0], #B matrix is strain displacement matrix for x and y
                             [0, zeta_py[0], 0, zeta_py[1], 0, zeta_py[2]],
                             [zeta_py[0], zeta_px[0], zeta_py[1], zeta_px[1], zeta_py[2], zeta_px[2]]])
     
-
     Ke = np.mat(np.zeros((6, 6)))
-
-    Ke = (B.T * D * B) * A * th
+    Ke = (B.T * D * B) * A * th #K matrix, element stiffness matrix
 
 
     if eq is None:
         return Ke
     else:
         fe = np.mat(np.zeros((6,1)))
+        fx = A * th * eq[0]/ 3.0
+        fy = A * th * eq[1]/ 3.0
+        fe = np.mat([[fx],[fy],[fx],[fy],[fx],[fy]])
         return Ke, fe
-    
+        
 def zeta_partials_x_and_y(ex,ey):
     """
     Compute partials of area coordinates with respect to x and y.
@@ -70,7 +64,7 @@ def zeta_partials_x_and_y(ex,ey):
     :param list ex: element x coordinates [x1, x2, x3]
     :param list ey: element y coordinates [y1, y2, y3]
     """
-    
+
     tmp = np.matrix([[1,ex[0],ey[0]],
                      [1,ex[1],ey[1]],
                      [1,ex[2],ey[2]]])
@@ -82,27 +76,19 @@ def zeta_partials_x_and_y(ex,ey):
     zeta_px = np.zeros(3)           # Partial derivative with respect to x
     zeta_py = np.zeros(3)           # Partial derivative with respect to y
 
-    # TODO: fill out missing parts (or reformulate completely)
    
-    a_i = []
-    v_i = []
-    c_i = []
-    for i in range(0,2): #Hvilken range? 
-        a_i.append(ex[cyclic_ijk[i+1]]*ey[cyclic_ijk[i+2]] - ex[cyclic_ijk[i+2]]*ey[cyclic_ijk[i+1]])
-        v_i.append(ey[cyclic_ijk[i+1]]-ey[cyclic_ijk[i+2]])
-        c_i.append(ex[cyclic_ijk[i+2]]-ex[cyclic_ijk[i+1]])
-        #A_i?
+    for i in range(3): 
+        j = cyclic_ijk[i+1]
+        k = cyclic_ijk[i+2]
+        zeta_px[i] = (ey[j] - ey[k]) / A2
+        zeta_py[i] = (ex[k] - ex[j]) / A2
 
-    for i in range(0,2):
-        zeta_px[i] = v_i[i]/A2
-        zeta_py[i] = c_i[i]/A2
-     
     return zeta_px, zeta_py
 
 
 # Functions for 6 node triangle
     
-def tri6_area(ex,ey):
+def tri6_area(ex,ey): #Function that calculates area of a triangle
         
     tmp = np.matrix([[1,ex[0],ey[0]],
                      [1,ex[1],ey[1]],
@@ -110,32 +96,44 @@ def tri6_area(ex,ey):
     
     A = np.linalg.det(tmp) / 2
     
+ 
     return A
 
 
 def tri6_shape_functions(zeta):
-    
+    '''
+    Compution shape functions of a 6 node element 
+    :param list zeta: Area coordinates
+    :return list N6: shape-functions/interpolation-functions 
+    '''
+
     cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
 
     N6 = np.zeros(6)
 
-    # TODO: fill out missing parts (or reformulate completely) 
-    '''DONE?'''
-
-    for i in range(0,2):
-       z = zeta[i]
-       N6.append(z*2(z-1))
-    
-    for i in range(0,2):
+    #Shape functions for nodes 1-3, separated because they are calculated differently
+    for i in range(3):
+        N6[i] = zeta[i] * (zeta[i] - 0.5) * 2.0
+        
+    #Shape functions for nodes 4-6
+    for i in range(3):
         j = cyclic_ijk[i+1]
-        N6.append(4*zeta[i]*zeta[j])
-
+        k = cyclic_ijk[i+2]  
+        N6[i+3] = zeta[i] * zeta[j] * 4.0
 
     return N6
 
 
 def tri6_shape_function_partials_x_and_y(zeta,ex,ey):
-    
+    '''
+    Computing partial derivatives of shape functions with respect to x and y
+    :param list zeta: Area coordinates
+    :param list ex: element x coordinates [x1, x2, x3, x4, x5, x6]
+    :param list ey: element y coordinates [y1, y2, y3, y4, y5, y6]
+    :return list N6_px: partial derivatives with respect to x of shape functions 
+    :return list N6_py: partial derivatives with respect to y of shape functions 
+    '''
+
     zeta_px, zeta_py = zeta_partials_x_and_y(ex,ey)
     
     N6_px = np.zeros(6)
@@ -143,61 +141,85 @@ def tri6_shape_function_partials_x_and_y(zeta,ex,ey):
     
     cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
 
-    # TODO: fill out missing parts (or reformulate completely)
-    ''' DONE? '''
-    N6_px = tri6_shape_functions(zeta_px)
-    N6_py = tri6_shape_functions(zeta_py)
-
+    #Computing partial derivatives from formula 
+    for i in range(3):
+        j = cyclic_ijk[i+1]
+        N6_px[i] = (4 * zeta[i] - 1) * zeta_px[i]
+        N6_py[i] = (4 * zeta[i] - 1) * zeta_py[i]
+        N6_px[i+3] = 4 * zeta[j] * zeta_px[i] + 4 * zeta[i] * zeta_px[j]
+        N6_py[i+3] = 4 * zeta[j] * zeta_py[i] + 4 * zeta[i] * zeta_py[j]
 
     return N6_px, N6_py
 
 
 def tri6_Bmatrix(zeta,ex,ey):
+    """
+    Calculating the strain displacement matrix
+    :param list zeta: Area coordinates for the respective nodes in the 6-node element, e.g [0.5 0.5 0.]
+    :param list ex: element x coordinates [x1, x2, x3, x4, x5, x6]
+    :param list ey: element y coordinates [y1, y2, y3, y4, y5, y6]
+    :return matrix B: Strain displacement matrix
+    """
     
     nx,ny = tri6_shape_function_partials_x_and_y(zeta, ex, ey)
 
-    Bmatrix = np.matrix(np.zeros((3,12)))
+    Bmatrix = np.matrix([
+        [nx[0], 0, nx[1], 0, nx[2], 0, nx[3], 0, nx[4], 0, nx[5], 0],
+        [0, ny[0], 0, ny[1], 0, ny[2], 0, ny[3], 0, ny[4], 0, ny[5]],
+        [ny[0], nx[0], ny[1], nx[1], ny[2], nx[2], ny[3], nx[3], ny[4], nx[4], ny[5], nx[5]]])
 
-    # TODO: fill out missing parts (or reformulate completely)
-    ''' DONE ? '''
-    A = tri6_area(ex,ey)
-    iA2 = 1/(2*A) #i before A2 because it's the inverse of A2
-
-    B  = (iA2)*np.array([[nx[0], 0, nx[1], 0, nx[2], 0],
-                  [0, ny[0], 0, ny[1], 0, ny[2]],
-                  [ny[0],nx[0],ny[1],nx[1],ny[2],nx[2]]])
 
     return Bmatrix
 
 
 def tri6_Kmatrix(ex,ey,D,th,eq=None):
+    '''
+    Calculating element stiffness matrix for 6-node triangle
+    :param list ex: element x coordinates [x1, x2, x3, x4, x5, x6]
+    :param list ey: element y coordinates [y1, y2, y3, y4, y5, y6]
+    :param list D : 2D constitutive matrix
+    :param list th: element thickness
+    :param list eq: distributed loads, local directions [bx, by]
+    :return matrix K: element stiffness matrix [12x12]
+    :return matrix fe: consistent load vector [12 x 1] (if eq!=None)
+
+    '''
     
-    zetaInt = np.array([[0.5,0.5,0.0],
+    zetaInt = np.array([[0.5,0.5,0.0], #Areacoordinates for node 4,5 and 6
                         [0.0,0.5,0.5],
                         [0.5,0.0,0.5]])
     
-    '''HVA ER DENNE VERDIEN OG SKAL DEN INN I B? '''
-    wInt = np.array([1.0/3.0,1.0/3.0,1.0/3.0])
+    wInt = np.array([1.0/3.0,1.0/3.0,1.0/3.0]) #Weights for the gauss points used in the integration 
 
-    A    = tri6_area(ex,ey)
+    A    = tri6_area(ex,ey) #Area of triangles 
     
-    Ke = np.matrix(np.zeros((12,12)))
+    Ke = np.zeros((12, 12)) #Creating empty array of zeroes with 12x12 dimention, will be filled by values to become the stiffness matrix
+    fe = np.zeros((12, 1)) #Creating empty array of zeroes that will be the consistent load vector
+    
 
-    # TODO: fill out missing parts (or reformulate completely)
-    B = tri6_Bmatrix(zetaInt, ex, ey)
+    for iGauss in range(3): #Numerical integration of the elemental stiffness matrix and load vector 
+        zeta = zetaInt[iGauss]
 
+        B = tri6_Bmatrix(zeta, ex, ey) 
+        Ke += (B.T @ D @ B) * A * th * wInt[iGauss] #Adding stiffness matrix of every iteration
 
-    Ke = (B.T * D * B)* A * th
+        if eq is not None:
+            f = np.array([[eq[0]], [eq[1]]]) #load vector 
+            N6 = tri6_shape_functions(zeta)
+            N2 = np.zeros((2, 12)) #displacement interpolation matrix
+
+        for i in range(6):
+            N2[0, i * 2] = N6[i]
+            N2[1, 1 + i * 2] = N6[i]
+
+        fe += N2.T @ f * A * wInt[iGauss] #numerical integration of load vector 
 
     if eq is None:
         return Ke
     else:
-        fe = np.matrix(np.zeros((12,1)))
-        ''' HVA SKJER OM DET ER EN FORDELT LAST PÅ? ''' 
-        # TODO: fill out missing parts (or reformulate completely)
-
         return Ke, fe
 
 def tri6e(ex,ey,D,th,eq=None):
     return tri6_Kmatrix(ex,ey,D,th,eq)
+
 
